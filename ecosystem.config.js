267e0bc87baef1e3ede95ed6463070ac9deb7d5f
Oliver Modinger <modingeroliver@gmail.com>
2026-02-22 17:35:28 -0300

    fix prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,30 @@
 // ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: 'next-frontend',
-        cwd: './frontend',
-        script: 'npm',
-        args: 'start -- -p 3000',
-      },
-      {
-        name: 'nest-backend',
-        cwd: './backend',
-        script: 'dist/main.js',
-        interpreter: 'node',
-      },
-    ],
-  };
+  apps : [
+    {
+      name: "backend-nest",
+      script: "dist/main.js",
+      cwd: "./backend",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001
+      }
+    },
+    {
+      name: "frontend-next",
+      script: "npm",
+      args: "start",
+      cwd: "./modern-landing-page",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
