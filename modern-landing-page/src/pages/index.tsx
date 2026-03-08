b2f5fe649f863963744aea10f64f6a741a89cd14
Oliver Modinger <modingeroliver@gmail.com>
2026-03-07 22:02:00 -0300

    fix icon

--- modern-landing-page/src/pages/index.tsx
+++ modern-landing-page/src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Head>
         <title>Voz en Letras</title>
         <meta name="description" content="Transcripción de audios a texto" />
-        <link rel="icon" href="../../public/logoFooter.png" />
+        <link rel="icon" href="../../public/LogoFooter.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
